c02c10b133aa865a2ae8ce89993a9285dfb1ac2c
bedeffinian <bedeffinian@gmail.com>
2021-06-23 20:08:07 +0100

    Change insert row and add completeFun()

--- R/InsertRow.R
+++ R/InsertRow.R
@@ -5,9 +5,9 @@
 #' @keywords Insert
 #' @export
 #' @examples
-#' Insert_Row()
+#' insertRow()
 
-Insert_Row <- function(existingDF, newrow, r) {
+insertRow <- function(existingDF, newrow, r) {
   existingDF[seq(r+1,nrow(existingDF)+1),] <- existingDF[seq(r,nrow(existingDF)),]
   existingDF[r,] <- newrow
   existingDF
