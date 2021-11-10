d21c2962f5f7d5c706e08d7cf8fc567d2c37d9a0
Bede Davies <bede.davies@plymouth.ac.uk>
2021-11-10 14:21:25 +0000

    updated plotting function

--- R/plot_model_Bede.R
+++ R/plot_model_Bede.R
@@ -1,50 +1,53 @@
 #' Model Plot Function
 #'
-#' This Function creates a ggplot of Model Predictions using YearNum and Area
+#' This Function creates a ggplot of Model Predictions using a X variable (xvar) and treatment (colourvar)
 #' @param plot used to create ggplot scales
 #' @keywords Model Predictions
 #' @export
 #' @examples
-#' plot_model_Bede_Orkney()
+#' plot_model_Bede()
 
 
 
-plot_model_Bede<-function(df_mean,df_pred,Metric,Formula,x){
+plot_model_Bede<-function(df_mean,df_pred,Metric,Formula,xvar,colourvar,pallette,xlabel){
 
+
+  xvar <- enquo(xvar)
+  colourvar <- enquo(colourvar)
 
   ggplot2::ggplot()+
-    ggplot2::geom_errorbar(data={{df_mean}},
-                           mapping=ggplot2::aes(x={{x}},
+    ggplot2::geom_errorbar(data=df_mean,
+                           mapping=ggplot2::aes(x=.data[[xvar]],
                                                 y=Mean,
                                                 ymin=Mean-CI,
                                                 ymax=Mean+CI,
-                                                colour=Area),
+                                                colour=.data[[colourvar]]),
                   size=1,width=0.1,
                   position = ggplot2::position_dodge(0.1))+
-    ggplot2::geom_point(data={{df_mean}},
-                        mapping=ggplot2::aes(x={{x}},
+    ggplot2::geom_point(data=df_mean,
+                        mapping=ggplot2::aes(x=.data[[xvar]],
                                              y=Mean,
-                                             shape=Area,
-                                             fill=Area),
+                                             shape=.data[[colourvar]],
+                                             fill=.data[[colourvar]]),
                         colour="black",
                position = ggplot2::position_dodge(0.1), size=2)+
-    ggplot2::geom_ribbon(data={{df_pred}},
-                         mapping=ggplot2::aes(x={{x}},
+    ggplot2::geom_ribbon(data=df_pred,
+                         mapping=ggplot2::aes(x=.data[[xvar]],
                                               y=fit_resp,
                                               ymin=lwr_resp,
                                               ymax=upr_resp,
-                                              colour=Area,
-                                              fill=Area),
+                                              colour=.data[[colourvar]],
+                                              fill=.data[[colourvar]]),
                 linetype='twodash',
                 alpha=0.2,size=0.5)+
-    ggplot2::geom_line(data={{df_pred}},
-                       mapping=ggplot2::aes(x={{x}},
+    ggplot2::geom_line(data=df_pred,
+                       mapping=ggplot2::aes(x=.data[[xvar]],
                                             y=fit_resp,
-                                            colour=Area,
-                                            linetype=Area))+
-    ggplot2::labs(y=paste0(stringr::str_to_title(Metric)),x=)+
+                                            colour=.data[[colourvar]],
+                                            linetype=.data[[colourvar]]))+
+    ggplot2::labs(y=paste0(stringr::str_to_title(Metric)),x=xlabel)+
     scale_colour_Bede(pallette)+
-    scale_fill_Bede()+
+    scale_fill_Bede(pallette)+
     theme_Bede()+
     guides(linetype=ggplot2::guide_legend(override.aes=list(fill=NA)))+
     ggtitle(paste0(stringr::str_to_title(Metric),": ",deparse(Formula)))
