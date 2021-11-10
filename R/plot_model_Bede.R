1eca9904d6ced2449411fa68b1493c4c015ad73d
Bede Davies <bede.davies@plymouth.ac.uk>
2021-11-10 13:25:30 +0000

    remove old functions

--- R/plot_model_Bede.R
+++ R/plot_model_Bede.R
@@ -9,27 +9,27 @@
 
 
 
-plot_model_Bede_Orkney<-function(df_mean,df_pred,Metric,Formula){
+plot_model_Bede<-function(df_mean,df_pred,Metric,Formula,x){
 
 
   ggplot2::ggplot()+
-    ggplot2::geom_errorbar(data=df_mean,
-                           mapping=ggplot2::aes(x=YearNum,
+    ggplot2::geom_errorbar(data={{df_mean}},
+                           mapping=ggplot2::aes(x={{x}},
                                                 y=Mean,
                                                 ymin=Mean-CI,
                                                 ymax=Mean+CI,
                                                 colour=Area),
                   size=1,width=0.1,
                   position = ggplot2::position_dodge(0.1))+
-    ggplot2::geom_point(data=df_mean,
-                        mapping=ggplot2::aes(x=YearNum,
+    ggplot2::geom_point(data={{df_mean}},
+                        mapping=ggplot2::aes(x={{x}},
                                              y=Mean,
                                              shape=Area,
                                              fill=Area),
                         colour="black",
                position = ggplot2::position_dodge(0.1), size=2)+
-    ggplot2::geom_ribbon(data=df_pred,
-                         mapping=ggplot2::aes(x=YearNum,
+    ggplot2::geom_ribbon(data={{df_pred}},
+                         mapping=ggplot2::aes(x={{x}},
                                               y=fit_resp,
                                               ymin=lwr_resp,
                                               ymax=upr_resp,
@@ -37,14 +37,14 @@ plot_model_Bede_Orkney<-function(df_mean,df_pred,Metric,Formula){
                                               fill=Area),
                 linetype='twodash',
                 alpha=0.2,size=0.5)+
-    ggplot2::geom_line(data=df_pred,
-                       mapping=ggplot2::aes(x=YearNum,
+    ggplot2::geom_line(data={{df_pred}},
+                       mapping=ggplot2::aes(x={{x}},
                                             y=fit_resp,
                                             colour=Area,
                                             linetype=Area))+
-    ggplot2::labs(y=paste0(stringr::str_to_title(Metric)),x="Year")+
-    scale_colour_Bede("Orkney")+
-    scale_fill_Bede("Orkney")+
+    ggplot2::labs(y=paste0(stringr::str_to_title(Metric)),x=)+
+    scale_colour_Bede(pallette)+
+    scale_fill_Bede()+
     theme_Bede()+
     guides(linetype=ggplot2::guide_legend(override.aes=list(fill=NA)))+
     ggtitle(paste0(stringr::str_to_title(Metric),": ",deparse(Formula)))
