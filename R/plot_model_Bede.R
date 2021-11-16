0fc15390d332f29aed477ca8d1f92dcc0fe44720
Bede Davies <bede.davies@plymouth.ac.uk>
2021-11-16 14:45:49 +0000

    added y name element

--- R/plot_model_Bede.R
+++ R/plot_model_Bede.R
@@ -9,7 +9,7 @@
 
 
 
-plot_model_Bede<-function(df_mean,df_pred,Metric,Formula,xvar,colourvar,pallette,xlabel){
+plot_model_Bede<-function(df_mean,df_pred,Metric,Name,Formula,xvar,colourvar,pallette,xlabel){
 
 
   ggplot2::ggplot()+
@@ -48,7 +48,7 @@ plot_model_Bede<-function(df_mean,df_pred,Metric,Formula,xvar,colourvar,pallette
     theme_Bede()+
     guides(linetype=ggplot2::guide_legend(override.aes=list(fill=NA)))+
     ggtitle(paste0(stringr::str_to_title(Metric),": ",deparse(Formula)))+
-    scale_y_continuous(name=paste0(stringr::str_to_title(Metric)),
+    scale_y_continuous(name=paste0(stringr::str_to_title(Name)),
                        limits = c((min(df_mean$Mean)-max(df_mean$CI)),
                                   (max(df_mean$Mean)+max(df_mean$CI))),
                        oob = scales::squish)
