2118c9154de5d6e93152cbf3ea319b058f0a2123
BedeFfinian <bedeffinian@gmail.com>
2022-08-18 16:30:41 +0200

    change the map theme to include gridlines on top

--- R/theme_Bede_Map.R
+++ R/theme_Bede_Map.R
@@ -10,22 +10,26 @@
 
 theme_Bede_Map <- function(...){
 
-  ggplot2::theme(plot.background = ggplot2::element_rect(fill = "transparent", colour = NA),
-          legend.key = ggplot2::element_rect(fill = "white"),
-          panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
-          legend.position=c(.99,.98),
-          legend.justification = c("right", "top"),
-          legend.box.just = "right",
-          legend.title = ggplot2::element_text(size=14),
-          legend.text = ggplot2::element_text(size=12),
-          panel.background = ggplot2::element_rect(fill = "lightblue"), # bg of the panel
-          panel.grid.major = ggplot2::element_blank(), # get rid of major grid
-          panel.grid.minor = ggplot2::element_blank(), # get rid of legend panel bg,
-          axis.text.x = ggplot2::element_text(size = 14,
-                                     colour = "black"),
-          axis.text.y = ggplot2::element_text(colour = "black",
-                                     size = 14),
-          axis.title = ggplot2::element_text(colour = "black",
-                                    size = 15)
-    )
+  ggplot2::theme(
+    plot.background = ggplot2::element_rect(fill = "transparent", colour = NA),
+    legend.key = ggplot2::element_rect(fill = "white"),
+    panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+    legend.position=c(.99,.98),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    legend.title = ggplot2::element_text(size=14),
+    legend.text = ggplot2::element_text(size=12),
+    panel.background = ggplot2::element_blank(), # bg of the panel
+    panel.grid.major = ggplot2::element_line(linetype = "dotted",
+                                             colour="grey30",
+                                             size=0.25),
+    panel.ontop = TRUE,
+    panel.grid.minor = ggplot2::element_blank(), # get rid of legend panel bg,
+    axis.text.x = ggplot2::element_text(size = 14,
+                                        colour = "black"),
+    axis.text.y = ggplot2::element_text(colour = "black",
+                                        size = 12),
+    axis.title = ggplot2::element_text(colour = "black",
+                                       size = 12)
+  )
 }
