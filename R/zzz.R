4d731bb9972d921d617df4f70f612d795a120442
bedeffinian <bedeffinian@gmail.com>
2026-05-12 16:34:18 +0200

    stopping the use of rlang

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,9 @@
 .onLoad <- function(lib, pkg) {
-  run_on_load()
-}
-
-on_load({
 
   sysfonts::font_add_google(name = "Poppins", family = "poppins")
   showtext::showtext_auto()
 
-})
+}
 
 .onUnload <- function(libpath) {
 
